f6cfb582e22abe9f61d541898107ee9845a8a8b9
abanoub nagib <abanoub.nagib@hotmail.com>
2023-11-28 21:50:40 -0800

    restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,6 @@
+const jwt = require('jsonwebtoken')
+
 module.exports = (req, res, next) => {
-  next();
   /*
     IMPLEMENT
 
@@ -11,4 +12,20 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
+  const token = req.headers.authorization
+
+  if (token) {
+    const secret = process.env.JWT_SECRET || "shh"
+    
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "token invalid" })
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
+  } else {
+    res.status(401).json({message: "token required" })
+  }
 };
