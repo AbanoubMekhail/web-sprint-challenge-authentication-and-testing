9fe5920ee107bef10c95aaaa865f24854e80e3ae
abanoub nagib <abanoub.nagib@hotmail.com>
2023-11-28 19:08:05 -0800

    sanity test

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,13 @@
+const server = require('./server');
+const request = require('supertest');
+
 // Write your tests here
 test('sanity', () => {
   expect(true).toBe(false)
+})
+
+describe('GET /', () => {
+  it('has process.env.NODE_ENV as "testing"', () => {
+    expect(process.env.NODE_ENV).toBe('testing')
+  })
 })
